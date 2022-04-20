0152fa2e14ca2fe65b511f128b9d3d2682b581b3
Marnikilis <diana@cravter.com>
2022-04-20 15:48:40 +0300

    add select

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,11 +11,13 @@ const Header = () => {
             <img src='https://instaread.co/images/instaread_logo_grey.svg' className={styles.logo} alt='logo'/>
           </NavLink>
             </div>
-          <div>
+          <div className={styles.nav}>
             <NavLink to='/discover'>Discover</NavLink>
             <NavLink to='/search'>Search</NavLink>
             <NavLink to='/try'>Try Instaread</NavLink>
-            <NavLink to='/login'>Login</NavLink>
+            <NavLink to='/login' className={styles.login}> Login
+              <button className={styles.select}/>
+            </NavLink>
       </div>
       </div>
     </header>
